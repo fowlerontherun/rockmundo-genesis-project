bf9babe2f736104e802b5da21c5ce9119b4ce9cb
fowlerontherun <81326019+fowlerontherun@users.noreply.github.com>
2025-09-17 17:42:02 +0100

    Redirect character create alias

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
 import Index from "./pages/Index";
 import PerformGig from "./pages/PerformGig";
@@ -89,7 +89,10 @@ function App() {
                 <Route path="songs" element={<SongManager />} />
                 <Route path="inventory" element={<InventoryManager />} />
                 <Route path="statistics" element={<PlayerStatistics />} />
-                <Route path="character/create" element={<CharacterCreationPage />} />
+                <Route
+                  path="character/create"
+                  element={<Navigate to="/character-create" replace />}
+                />
               </Route>
               <Route path="*" element={<NotFound />} />
             </Routes>
