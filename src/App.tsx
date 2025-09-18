f6648c763e4217c52939469b7f66f616cb310357
fowlerontherun <81326019+fowlerontherun@users.noreply.github.com>
2025-09-18 11:35:26 +0100

    Lazy load Busking page to avoid duplicate import

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
 import { GameDataProvider } from "./hooks/useGameData";
-import Busking from "./pages/Busking";
 
 const Layout = lazy(() => import("./components/Layout"));
 const Index = lazy(() => import("./pages/Index"));
@@ -44,7 +43,7 @@ const WorldEnvironment = lazy(() => import("./pages/WorldEnvironment"));
 const SongManager = lazy(() => import("./pages/SongManager"));
 const InventoryManager = lazy(() => import("./pages/InventoryManager"));
 const PlayerStatistics = lazy(() => import("./pages/PlayerStatistics"));
-import Busking from "./pages/Busking";
+const Busking = lazy(() => import("./pages/Busking"));
 
 const queryClient = new QueryClient();
 
