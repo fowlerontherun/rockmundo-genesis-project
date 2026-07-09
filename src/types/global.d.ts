11deaf87b0dce5012daa5f9bfb4c5b739ea7e119
fowlerontherun <81326019+fowlerontherun@users.noreply.github.com>
2026-07-09 08:02:14 +0100

    Stabilize TypeScript build issues

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,3 +5,23 @@ declare global {
     Tone?: unknown;
   }
 }
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
