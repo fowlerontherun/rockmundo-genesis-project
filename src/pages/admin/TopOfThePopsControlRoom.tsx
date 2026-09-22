93047031923c38ae8923f1c669a6e8d27a1dd65e
fowlerontherun <81326019+fowlerontherun@users.noreply.github.com>
2026-09-22 13:10:57 +0100

    Add TOTP booking manager to control room

--- src/pages/admin/TopOfThePopsControlRoom.tsx
+++ src/pages/admin/TopOfThePopsControlRoom.tsx
@@ -13,6 +13,7 @@ import { TotpComplianceCard } from "@/features/top-of-the-pops/TotpComplianceCar
 import { TotpYoutubePublishCard } from "@/features/top-of-the-pops/TotpYoutubePublishCard";
 import { TotpBroadcastContractCard } from "@/features/top-of-the-pops/TotpBroadcastContractCard";
 import { TotpAudioStudio } from "@/features/top-of-the-pops/TotpAudioStudio";
+import { TotpBookingManager } from "@/features/top-of-the-pops/TotpBookingManager";
 import { resolveTotpPresenter } from "@/features/top-of-the-pops/presenters";
 
 export default function TopOfThePopsControlRoom() {
@@ -64,6 +65,7 @@ export default function TopOfThePopsControlRoom() {
         </Card>
       ) : current ? (
         <>
+          <TotpBookingManager episodeId={current.id} />
           <TotpControlRoomCard episode={current} />
           <TotpRunningSheetCard episode={current} />
           <TotpAudioStudio episode={current} />
